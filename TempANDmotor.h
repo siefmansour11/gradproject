abb701db0df9ebf48bdd43bcd4032f76aa9ecd9d
siefmansour11 <siefmansoursys@gmail.com>
2025-02-21 13:56:19 +0200

    Create TempANDmotor.h

---
+++ TempANDmotor.h
@@ -0,0 +1,17 @@
+/* 
+ * File:   TempANDmotor.h
+ * Author: safe
+ *
+ * Created on January 31, 2025, 4:43 PM
+ */
+
+
+#ifndef TEMPANDMOTOR_H
+#define TEMPANDMOTOR_H
+
+#include <stdint.h>
+
+void FlameSensor_init();
+uint8_t FlameSensor_Read();
+
+#endif
