a3c79aebb5e4f43a9e526776e88182056b8a42fc
siefmansour11 <siefmansoursys@gmail.com>
2025-02-21 13:42:51 +0200

    Create Bluetooth.h

---
+++ Bluetooth.h
@@ -0,0 +1,13 @@
+/* 
+ * File:   Bluetooth.h
+ * Author: safe
+ *
+ * Created on January 31, 2025, 4:50 PM
+ */
+#ifndef BLUETOOTH_H
+#define BLUETOOTH_H
+
+void Bluetooth_init();
+char Bluetooth_Receive();
+
+#endif
