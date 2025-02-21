24fcb0c0c857622b96319783b2ef07e82aff5d28
siefmansour11 <siefmansoursys@gmail.com>
2025-02-21 13:53:07 +0200

    Create motor.h

---
+++ motor.h
@@ -0,0 +1,15 @@
+/* 
+ * File:   motor.h
+ * Author: safe
+ *
+ * Created on February 14, 2025, 7:29 PM
+ */
+
+#ifndef MOTOR_H
+#define	MOTOR_H
+
+void Motor_Init();
+void Motor_On();
+void Motor_Off();
+#endif	/* MOTOR_H */
+
